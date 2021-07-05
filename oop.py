9fa06f697e8702ed12b4a0b90805211ce852ca30
rafalsz <kw3rty96@gmail.com>
2021-07-05 23:21:14 +0200

    Add Kettle class to oop.py file

--- oop.py
+++ oop.py
@@ -1,3 +1,35 @@
+class Kettle(object):
+
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        self.on = False
+
+    def switch_on(self):
+        self.on = True
 
 
-a = 5
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.name)
+print(kenwood.price)
+
+kenwood.price = 12.75
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+
+print("Models: {} = {}, {} = {}".format(kenwood.name, kenwood.price, hamilton.name, hamilton.price))
+
+print("Models: {0.name} = {0.price}, {1.name} = {1.price}".format(kenwood, hamilton))
+
+print(kenwood.on)
+kenwood.switch_on()
+print(kenwood.on)
+
+Kettle.switch_on(kenwood)
+print(kenwood.on)
+
+# class can be dynamic modified by doing something like this below:
+kenwood.power = 1.5
+print(kenwood.power)
+print(hamilton.power)
