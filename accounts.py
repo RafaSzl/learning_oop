7f6f303f23a4cc53d88ea3e80ea70e96dab85353
rafalsz <kw3rty96@gmail.com>
2021-07-10 16:39:11 +0200

    Add new client by use Account class

--- accounts.py
+++ accounts.py
@@ -9,42 +9,43 @@ class Account:
     def _current_time():
         # w dwoch krokach mam czas lokalny: 1. modul datetime pokazuje date w utc
         # i potem pytz zamienia to w czas lokalny
-        #undersore w tej method pokazuje ze to nie jest do uzycia poza klasa
+        # undersore w tej method pokazuje ze to nie jest do uzycia poza klasa
         utc_time = datetime.datetime.utcnow()
         return pytz.utc.localize(utc_time)
 
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        self.transaction_list = []
-        print("Account created for" + self.name)
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = [(Account._current_time(), balance)]
+        print("Account created for " + self._name)
+        # print("State of an account: " + str(self.__balance))
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
             # self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
             # i tutaj zamiast self._current_time() mam Account bo to jest static
             # method i dzieki temu python nie szuka po instancjach i pozniej po klasach tylko od razu po klasach
-            self.transaction_list.append((Account._current_time(), amount))
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if amount > self.balance:
+        if amount > self.__balance:
             print("Cant withdraw more than you have")
             self.show_balance()
         elif amount < 0:
             print("Cant withdraw values below 0")
             self.show_balance()
         else:
-            self.balance -= amount
+            self.__balance -= amount
             # self.show_balance()
-            self.transaction_list.append((Account._current_time(), -amount))
+            self._transaction_list.append((Account._current_time(), -amount))
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
 
     def show_transactions(self):
-        for date, amount in self.transaction_list:
+        for date, amount in self._transaction_list:
             if amount > 0:
                 tran_type = "deposited"
             else:
@@ -64,6 +65,8 @@ if __name__ == '__main__':
     tim.show_transactions()
 
     steph = Account("Steph", 800)
+    steph.__balance = 200
     steph.deposit(100)
     steph.withdraw(200)
     steph.show_transactions()
+    print(steph.__dict__)
