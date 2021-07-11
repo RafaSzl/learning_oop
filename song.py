36229d9013573a33c0f1887a4e25a860e06cb8f0
rafalsz <kw3rty96@gmail.com>
2021-07-11 16:13:12 +0200

    Add song.py file with class Song

---
+++ song.py
@@ -0,0 +1,21 @@
+class Song
+    """Class to represent a song
+    Attributes:
+        title (str)
+        artist (Artist)
+        duration (int)
+    
+    """
+    def __init__(self, title, artist, duration=0): # duration default to zero
+        """
+        Song init method
+        Args:
+        :param title: Initialises the 'title' attribute
+        :param artist: At Artist object representing the song's creator
+        :param duration: Initial value for the 'duration' attribute.
+                        Will default to zero if not specified
+        """
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+
