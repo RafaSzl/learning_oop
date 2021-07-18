be6cb4a63717b469305ddc8e1a37cf337982e685
rafalsz <kw3rty96@gmail.com>
2021-07-18 23:15:06 +0200

    Modify player.py and main.py to use in Player class  getters and setters, including properties
    In player.py was used properties method as well as briefly explained decorators

--- game/main.py
+++ game/main.py
@@ -1,3 +1,31 @@
-import player
+from player import Player
 
-tim = player.Player("Tim")
+tim = Player("Tim")
+print(tim.name)
+print(tim.lives)
+
+tim.lives -= 1
+tim.level += 1
+print(tim)
+
+tim.lives -= 1
+tim.level += 1
+print(tim)
+
+tim.lives -= 1
+tim.level += 7
+print(tim)
+
+tim.lives += 1
+print(tim)
+
+tim.score = 500
+print(tim)
+
+
+
+# # it is called getter, it gets a value from a data attribute (here data is a class Player)
+# print(tim.get_name())
+#
+# # it is called setter, it sets a value for a data attribute (here data is a class Player)
+# tim.set_lives(300)
