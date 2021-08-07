c135335817dc487ed4f6f0e67624b41f151969c2
rafalsz <kw3rty96@gmail.com>
2021-08-07 17:43:09 +0200

    Add duck.py file to understand common python phrase and introduction to polymorphism

---
+++ game/ducks.py
@@ -0,0 +1,57 @@
+class Wing(object):
+
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("great ratio")
+        if self.ratio == 1:
+            print("hard to fly, but fly")
+        if self.ratio < 1:
+            print("I think I'll just walk")
+
+
+class Duck(object):
+
+    def __init__(self):
+        self.wing = Wing(1.8)
+
+    def walk(self):
+        print("tap tap tap")
+
+    def swim(self):
+        print("duck is swimming")
+
+    def quack(self):
+        print("Quack")
+
+    def fly(self):
+        self._wing.fly()
+
+
+class Penguin(object):
+
+    def walk(self):
+        print("i can tap tap too")
+
+    def swim(self):
+        print("swim almost like a duck")
+
+    def quack(self):
+        print("Quack, but in penguinish")
+
+
+
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
+
+if __name__ == '__main__':
+    donald = Duck()
+    # test_duck(donald)
+    #
+    # percy = Penguin()
+    # test_duck(percy)
+
