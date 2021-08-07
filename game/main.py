ff4ef07f2417d7b181c34e8874346b782b7d637b
rafalsz <kw3rty96@gmail.com>
2021-08-07 17:40:46 +0200

    create subclasses in Enemy class in enemy.py: Vampyre and VampyreKing and checking them into main.py
    Vampyre and VampyreKing have new method: dodging and have modified take_damage method inherited from Enemy superclass. These methods were checked in main.py file and after that the checking lines were changed into comments. Right after i added some code to start learning polymorphism

--- game/main.py
+++ game/main.py
@@ -1,39 +1,63 @@
-from player import Player
-from enemy import Enemy, Troll
-
-tim = Player("Tim")
-print(tim.name)
-print(tim.lives)
-
-tim.lives -= 1
-tim.level += 1
-print(tim)
-
-random_monster = Enemy("Basic enemy", 12, 1)
-print(random_monster)
-
-random_monster.take_damage(4)
-print(random_monster)
-
-random_monster.take_damage(14)
-print(random_monster)
-
-random_monster.take_damage(4)
-print(random_monster)
-
-ugly_troll = Troll("Pug")
-print("ugly troll - {}".format(ugly_troll))
-
-another_troll = Troll("Ug")
-print("Another troll - {}".format(another_troll))
-
-brother = Troll("Urg")
-print("brother - {}".format(another_troll))
-
-ugly_troll.grunt()
-
+# from player import Player
+# from enemy import Enemy, Troll, Vampyre, Vampyreking
+#
+# tim = Player("Tim")
+# print(tim.name)
+# print(tim.lives)
+#
+# tim.lives -= 1
+# tim.level += 1
+# print(tim)
+#
+# random_monster = Enemy("Basic enemy", 12, 1)
+# print(random_monster)
+#
+# random_monster.take_damage(4)
+# print(random_monster)
+#
+# random_monster.take_damage(14)
+# print(random_monster)
+#
+# random_monster.take_damage(4)
+# print(random_monster)
+#
+# ugly_troll = Troll("Pug")
+# print("ugly troll - {}".format(ugly_troll))
+#
+# another_troll = Troll("Ug")
+# print("Another troll - {}".format(another_troll))
+#
+# brother = Troll("Urg")
+# print("brother - {}".format(another_troll))
+#
+# ugly_troll.grunt()
+#
+# dracula = Vampyre("Dracula")
+# dracula.take_damage(10)
+# print("Dracula - {}".format(dracula))
+# print(dracula)
+#
+# while dracula._alive:
+#     dracula.take_damage(1)
+#
+# Kingofvamp = Vampyreking("Jozek")
+# Kingofvamp._lives = 10
+# Kingofvamp._hit_points = 140
+# print(Kingofvamp)
+# Kingofvamp.take_damage(13)
+    # print(dracula)
 # # it is called getter, it gets a value from a data attribute (here data is a class Player)
 # print(tim.get_name())
 #
 # # it is called setter, it sets a value for a data attribute (here data is a class Player)
 # tim.set_lives(300)
+
+# POLYMORPHISM
+
+a = 3
+b = 'tim'
+c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
