0fe727dc97df5c86a6fe49d77e14c03955d95f39
rafalsz <kw3rty96@gmail.com>
2021-08-10 12:54:34 +0200

    changing html_doc.py. Instead of composition we are using aggregation

--- game/html_doc.py
+++ game/html_doc.py
@@ -44,13 +44,31 @@ class Body(Tag):
 
         super().display(file=file)
 
+# COMPOSITION
+# class HtmlDoc(object):
+#
+#     def __init__(self, title=None):
+#         self._doc_type = DocType()
+#         self._head = Head(title)
+#         self._body = Body()
+#
+#     def add_tag(self, name, content):
+#         self._body.add_tag(name, content)
+#
+#     def display(self, file=None):
+#         self._doc_type.display(file=file)
+#         print('<html>', file=file)
+#         self._head.display(file=file)
+#         self._body.display(file=file)
+#         print('</html>', file=file)
 
+# AGGREGATION ( we defined separately doctype, head and body classes that they can exist without my_page
 class HtmlDoc(object):
 
-    def __init__(self, title=None):
-        self._doc_type = DocType()
-        self._head = Head(title)
-        self._body = Body()
+    def __init__(self, doc_type, head, body):
+        self._doc_type = doc_type
+        self._head = head
+        self._body = body
 
     def add_tag(self, name, content):
         self._body.add_tag(name, content)
@@ -64,10 +82,31 @@ class HtmlDoc(object):
 
 
 if __name__ == '__main__':
-    my_page = HtmlDoc('Demo doc')
-    my_page.add_tag('h1', 'Main heading')
-    my_page.add_tag('h2', 'sub-heading')
-    my_page.add_tag('p', 'This is a paragraph that will appear on the page')
-    with open('test.html', 'w') as test_doc:
+    # COMPOSITION
+    # my_page = HtmlDoc('Demo doc')
+    # my_page.add_tag('h1', 'Main heading')
+    # my_page.add_tag('h2', 'sub-heading')
+    # my_page.add_tag('p', 'This is a paragraph that will appear on the page')
+    # with open('test.html', 'w') as test_doc:
+    #     my_page.display(file=test_doc)
+
+    # AGGREGATION
+    new_body = Body()
+    new_body.add_tag('h1', 'Aggregation')
+    new_body.add_tag('p', "Unlike <strong>composition</strong>, aggregation uses existing instances"
+                     "of objects to build up another object.")
+    new_body.add_tag('p', "The composed object doesn't actually own the objects that it's composed of"
+                     " - if it is destroyed, those objects continue to exist.")
+
+    new_docType = DocType()
+    new_header = Head('Aggregation document')
+    my_page = HtmlDoc(new_docType, new_header, new_body)
+
+    with open('test3.html', 'w') as test_doc:
         my_page.display(file=test_doc)
 
+# COMPOSITION
+# # give our document content by switching it's body
+# my_page._body = new_body
+# with open('test2.html', 'w') as test_doc:
+#     my_page.display(file=test_doc)
